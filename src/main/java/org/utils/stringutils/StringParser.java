81d0e601ff17dd04b616675222a0c2156e2669bd
ShawnTanzc <E0406872@u.nus.edu>
2020-10-02 17:36:26 +0800

    fix checkstyle

--- src/main/java/org/utils/stringutils/StringParser.java
+++ src/main/java/org/utils/stringutils/StringParser.java
@@ -7,10 +7,12 @@ public class StringParser {
 
     /**
      * Splits the string based on the separator provided as parameter
+     * Splits the string based on the separator provided as parameter.
      *
      * @param stringToSplit the string to be split
      * @param separator    a string representing the separator that you want to split the string on
      * @return ArrayList of strings
+     * @throws InvalidStringInputException defined in the exceptions package.
      */
     public ArrayList<String> splitString(String stringToSplit, String separator) {
 
